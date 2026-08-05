8644130c32cebac39789efa496b4a590de44eb04
rammxyzzi <yannxyz832@gmail.com>
2026-08-05 20:11:45 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,3 +1,7 @@
+<!-- Supabase JS Client CDN -->
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+
+
 // --- KONFIGURASI SUPABASE ---
 const SUPABASE_URL = 'https://lwzeobrzpzjxiywdwuws.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3emVvYnJ6cHpqeGl5d2R3dXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0ODE2NDgsImV4cCI6MjEwMTA1NzY0OH0.kSQ3yvm2JwpPo4qRkRDxmEpkwrzpktGYBU0HAg3PXx4';
