d3d6a2dc702f47b20ed3192447e513517fc922de
rammxyzzi <yannxyz832@gmail.com>
2026-08-05 20:01:58 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 // --- KONFIGURASI SUPABASE ---
-const SUPABASE_URL = 'https://xyzcompany.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
+const SUPABASE_URL = 'https://lwzeobrzpzjxiywdwuws.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3emVvYnJ6cHpqeGl5d2R3dXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0ODE2NDgsImV4cCI6MjEwMTA1NzY0OH0.kSQ3yvm2JwpPo4qRkRDxmEpkwrzpktGYBU0HAg3PXx4';
 
 // Inisialisasi Klien Supabase global
 const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
