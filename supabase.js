36554e3d8c5a936ca065c69a2cd862bc3ff5c9e0
rammxyzzi <yannxyz832@gmail.com>
2026-08-05 20:01:17 +0700

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.0/+esm'
+// --- KONFIGURASI SUPABASE ---
+const SUPABASE_URL = 'https://xyzcompany.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...';
 
-const supabaseUrl = 'https://lwzeobrzpzjxiywdwuws.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx3emVvYnJ6cHpqeGl5d2R3dXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0ODE2NDgsImV4cCI6MjEwMTA1NzY0OH0.kSQ3yvm2JwpPo4qRkRDxmEpkwrzpktGYBU0HAg3PXx4'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// Inisialisasi Klien Supabase global
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
