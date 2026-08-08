615b4c964bc1dc30854a16e6be6e13281a4eebcb
rammxyzzi <yannxyz832@gmail.com>
2026-08-08 12:08:23 +0700

    Update supabase.js

--- config/supabase.js
+++ config/supabase.js
@@ -1,1 +1,6 @@
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 
+const supabaseUrl = 'https://XYZ.supabase.co'; // GANTI DENGAN URL SUPABASE KAMU
+const supabaseKey = 'eyJhbG...'; // GANTI DENGAN ANON KEY SUPABASE KAMU
+
+export const supabase = createClient(supabaseUrl, supabaseKey);
