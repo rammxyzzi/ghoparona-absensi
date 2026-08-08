41cbc737d86265d6dfb4bbfc9140a47a20d147f3
rammxyzzi <yannxyz832@gmail.com>
2026-08-08 12:06:34 +0700

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-
-const supabaseUrl = 'https://XYZ.supabase.co'; // GANTI DENGAN URL SUPABASE KAMU
-const supabaseKey = 'eyJhbG...'; // GANTI DENGAN ANON KEY SUPABASE KAMU
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
